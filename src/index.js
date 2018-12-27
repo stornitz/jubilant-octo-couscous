4ab96ea62ca311b3b9bec5fd55869ccc840dce25
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2018-12-27 12:51:25 +0100

    wrap server startup in a function and remove server.host config

--- src/index.js
+++ src/index.js
@@ -3,37 +3,47 @@ const http = require('http');
 // TODO check if file exists
 // TODO provide default configuration if not set
 const config = require('../config.json');
+const TrelloAPI = require('./trello-api.js');
 
-http.createServer((req, res) => {
-  // TODO check if the call is from Trello
-  if(req.method === 'POST') {
+function startServer() {
+  http.createServer((req, res) => {
+    // TODO check if the call is from Trello
+    if(req.method === 'POST') {
 
-    let data = '';
-    req.on('data', chunk => {
-      data += chunk;
-    });
+      let data = '';
+      req.on('data', chunk => {
+        data += chunk;
+      });
 
-    req.on('end', () => {
-      try {
-        const requestData = JSON.parse(data);
+      req.on('end', () => {
+        try {
+          const requestData = JSON.parse(data);
 
-        // TODO do something with the data
-        console.log(requestData);
+          // TODO do something with the data
+          console.log(requestData);
 
-        res.statusCode = 200;
-        res.end("ok");
-      } catch (e) {
-        res.statusCode = 400;
-        res.end('Invalid JSON');
-      }
-    })
+          res.statusCode = 200;
+          res.end("ok");
+        } catch (e) {
+          res.statusCode = 400;
+          res.end('Invalid JSON');
+        }
+      })
 
-  } else {
-    res.statusCode = 400;
-    res.end('Unsupported method, please POST a JSON object');
-  }
-}).listen(config.server.port, config.server.host, (err) => {
-  if(err) throw err;
+    // TODO check if the call is from Trello
+    } else if(req.method === 'HEAD') {
+      res.statusCode = 200;
+      res.end("ok");
 
-  console.log(`Server started at ${config.server.host}:${config.server.port}`);
-});
+    } else {
+      res.statusCode = 400;
+      res.end('Unsupported method, please POST a JSON object');
+    }
+  }).listen(config.server.port, err => {
+    if(err) throw err;
+
+    console.log(`Server started at 0.0.0.0:${config.server.port}`);
+  });
+}
+
+startServer();
