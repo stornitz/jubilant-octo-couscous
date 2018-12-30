d6be090f9fa067582efe8e370f9aa58d8878b2d6
Hugo Gallazzini <1359916+stornitz@users.noreply.github.com>
2018-12-30 13:03:44 +0100

    Add an option to use the checklist name defined in constants

--- src/triggers/checklist-complete.js
+++ src/triggers/checklist-complete.js
@@ -2,9 +2,13 @@ module.exports = (card, params, tools, constants) => {
   let atLeastOneChecklistChecked = false;
   let completeChecklist = true;
   let j = 0;
+  
+  if('checklist_name' in params) {
+    params.checklist_display_name = constants.checklists[params.checklist_name].display_name;
+  }
 
   while(completeChecklist && j < card.checklists.length) {
-    if(card.checklists[j].name == params.checklist_name) {
+    if(card.checklists[j].name == params.checklist_display_name) {
 
       atLeastOneChecklistChecked = true;
       let checkItems = card.checklists[j].checkItems;
